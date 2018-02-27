62f8eda4cd5802550cf78384fb05c4fa59cd1d8e
superihti <35236800+superihti@users.noreply.github.com>
2018-02-27 10:47:50 +0300

    Add files via upload

---
+++ build/js/scrypt.js
@@ -0,0 +1,15 @@
+$(function() {
+    var form = $('.analysis-form-container'),
+        openForm = $('.analysis__open-form'),
+        closeForm = $('.analysis-form-close__btn');
+    
+    openForm.on('click', function(){
+      form.show(300);
+  })  
+    closeForm.on('click', function(){
+      form.hide(300);
+  })  
+    
+});
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImZvcm0uanMiLCJtYWluLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FDWkEiLCJmaWxlIjoic2NyeXB0LmpzIiwic291cmNlc0NvbnRlbnQiOlsiJChmdW5jdGlvbigpIHtcclxuICAgIHZhciBmb3JtID0gJCgnLmFuYWx5c2lzLWZvcm0tY29udGFpbmVyJyksXHJcbiAgICAgICAgb3BlbkZvcm0gPSAkKCcuYW5hbHlzaXNfX29wZW4tZm9ybScpLFxyXG4gICAgICAgIGNsb3NlRm9ybSA9ICQoJy5hbmFseXNpcy1mb3JtLWNsb3NlX19idG4nKTtcclxuICAgIFxyXG4gICAgb3BlbkZvcm0ub24oJ2NsaWNrJywgZnVuY3Rpb24oKXtcclxuICAgICAgZm9ybS5zaG93KDMwMCk7XHJcbiAgfSkgIFxyXG4gICAgY2xvc2VGb3JtLm9uKCdjbGljaycsIGZ1bmN0aW9uKCl7XHJcbiAgICAgIGZvcm0uaGlkZSgzMDApO1xyXG4gIH0pICBcclxuICAgIFxyXG59KTsiLCIiXX0=
